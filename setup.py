acf84554ca81e13e58150253aab634577bd9555d
Alister <cranalister@icloud.com>
2020-11-18 13:00:04 -0800

    Setup.py file
    Added comments on License and left a comment space for any upload commands that we may need. Setup and Package meta-data has been set.

--- setup.py
+++ setup.py
@@ -1,122 +1,37 @@
-# Modified from https://github.com/kennethreitz/setup.py
+# Modified from https://github.com/CAMBI-tech/BciPy
 # Note: To use the 'upload' functionality of this file, you must:
-#   $ pip install twine
-# import os
-# import sys
-# from shutil import rmtree
+# $ pip install twine
+import os
+import sys
+from setuptools import find_packages, setup, Command
 
-# from setuptools import find_packages, setup, Command
+# Package meta-data.
+NAME = 'ExpSyncPy'
+DESCRIPTION = 'Facilitate experimental data sharing.'
+URL = 'https://github.com/CAMBI-tech/ExpSyncPy'
+EMAIL = 'memmott@ohsu.com'
+AUTHOR = 'CAMBI'
+REQUIRES_PYTHON = '~=3.6'
+VERSION = '0.0.0'
 
-# # Package meta-data.
-# NAME = 'bcipy'
-# DESCRIPTION = 'Python Software for Brain-Computer Interface.'
-# URL = 'https://github.com/BciPy/BciPy'
-# EMAIL = 'memmott@ohsu.com'
-# AUTHOR = 'Tab Memmott'
-# REQUIRES_PYTHON = '~=3.6'
+#Class UploadCommand  - waiting for further instructions 
 
-# VERSION = '1.4.2'
-
-
-# # What packages are required for this module to be executed?
-# with open('requirements.txt') as f:
-#     REQUIRED = f.read().splitlines()
-
-# # The rest you shouldn't have to touch too much :)
-# # ------------------------------------------------
-# # Except, perhaps the License and Trove Classifiers!
-# # If you do change the License, remember to change the Trove Classifier for that!
-
-# here = os.path.abspath(os.path.dirname(__file__))
-
-# long_description = 'Python Brain-Computer Interface Software'
-
-# about = {'__version__': VERSION}
-
-
-# class UploadCommand(Command):
-#     """Support setup.py upload."""
-
-#     description = 'Build and publish the package.'
-#     user_options = []
-
-#     @staticmethod
-#     def status(s):
-#         """Prints things in bold."""
-#         print('\033[1m{0}\033[0m'.format(s))
-
-#     def initialize_options(self):
-#         pass
-
-#     def finalize_options(self):
-#         pass
-
-#     def run(self):
-#         try:
-#             self.status('Removing previous builds…')
-#             rmtree(os.path.join(here, 'dist'))
-#         except OSError:
-#             pass
-
-#         self.status('Building Source and Wheel (universal) distribution…')
-#         os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-#         self.status('Uploading the package to PyPi via Twine…')
-#         os.system('twine upload dist/*')
-
-#         self.status('Pushing git tags…')
-#         os.system('git tag v{0}'.format(about['__version__']))
-#         os.system('git push --tags')
-
-#         sys.exit()
-
-
-# # Where the magic happens:
-# setup(
-#     name=NAME,
-#     version=about['__version__'],
-#     description=DESCRIPTION,
-#     long_description=long_description,
-#     long_description_content_type='text/markdown',
-#     author=AUTHOR,
-#     author_email=EMAIL,
-#     python_requires=REQUIRES_PYTHON,
-#     url=URL,
-#     packages=find_packages(exclude=('tests', 'demo', 'data', )),
-#     # If your package is a single module, use this instead of 'packages':
-#     # py_modules=['mypackage'],
-
-#     entry_points={
-#         'console_scripts': ['mycli=mymodule:cli'],
-#     },
-#     install_requires=REQUIRED,
-#     include_package_data=True,
-#     license='MIT',
-#     classifiers=[
-#         # Trove classifiers
-#         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-#         'License :: OSI Approved :: MIT License',
-#         'Programming Language :: Python',
-#         'Programming Language :: Python :: 3',
-#         'Programming Language :: Python :: 3.6',
-#         'Programming Language :: Python :: Implementation :: CPython',
-#         'Programming Language :: Python :: Implementation :: PyPy'
-#     ],
-#     # $ setup.py publish support.
-#     cmdclass={
-#         'upload': UploadCommand,
-#     },
-# )
+#Where the magic happens: 
 
 from setuptools import find_packages, setup
 setup(
-    name='expsyncpy',
-    packages=find_packages(),
-    version='0.0.0',
-    description='Facilitate experimental data sharing',
-    author='CAMBI',
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages()
     include_package_data=True,
-    license='',
+    license='', #Possible: MIT ? 
     install_requires=[],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'], 
