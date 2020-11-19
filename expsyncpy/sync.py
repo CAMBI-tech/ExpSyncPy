ab269fe40f4186ceaa70899b1b95e489dea56d03
Alister <cranalister@icloud.com>
2020-11-18 16:27:37 -0800

    Run autopep8
    Run autopep8 to fix lines and spacing issues in setup.py, setup.cfg, test_sync.py, and sunc.py files. Deleted meta-data requested, added comma, nd deleted line 30.

--- expsyncpy/sync.py
+++ expsyncpy/sync.py
@@ -1,7 +1,6 @@
-
-
 def place_holder(x, y):
     return x + y
+
 
 if __name__ == "__main__":
     ans = place_holder(4, 5)
--- setup.py
+++ setup.py
@@ -8,32 +8,23 @@ from setuptools import find_packages, setup, Command
 # Package meta-data.
 NAME = 'ExpSyncPy'
 DESCRIPTION = 'Facilitate experimental data sharing.'
-URL = 'https://github.com/CAMBI-tech/ExpSyncPy'
-EMAIL = 'memmott@ohsu.com'
-AUTHOR = 'CAMBI'
-REQUIRES_PYTHON = '~=3.6'
 VERSION = '0.0.0'
 
-#Class UploadCommand  - waiting for further instructions 
+# Class UploadCommand  - waiting for further instructions
 
-#Where the magic happens: 
+# Where the magic happens:
 
-from setuptools import find_packages, setup
 setup(
     name=NAME,
     version=about['__version__'],
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=find_packages()
+    packages=find_packages(),
     include_package_data=True,
-    license='', #Possible: MIT ? 
+    license='',  # Possible: MIT ?
     install_requires=[],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest==4.4.1'], 
+    tests_require=['pytest==4.4.1'],
     test_suite='tests',
 )
